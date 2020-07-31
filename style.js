257b08b5af19d8452c5b0f7807ec4ecf6919c0d5
AparisBlink <61824533+AparisBlink@users.noreply.github.com>
2020-07-31 11:51:11 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,110 @@
+(function (blink) {
+	'use strict';
+
+	var maristaschileStyle = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	maristaschileStyle.prototype = {
+		//BK-15873 añadimos el estilo basic como parent para la herencia de los estilos del CKEditor
+		parent: blink.theme.styles.basic.prototype,
+		bodyClassName: 'content_type_clase_maristaschile',
+		ckEditorStyles: {
+			name: 'maristaschile',
+			styles: [
+
+				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
+				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+
+				{ name: 'Lista 123 azul', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
+				{ name: 'Lista abc azul', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
+				{ name: 'Lista 123 roja', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
+				{ name: 'Lista 123 rosa', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
+				{ name: 'Lista abc azul claro', element: 'ol', attributes: { 'class': 'bck-ol bck-ol5' } },
+				{ name: 'Lista 123 azul', element: 'ol', attributes: { 'class': 'bck-ol bck-ol6' } },
+				{ name: 'Lista abc morada', element: 'ol', attributes: { 'class': 'bck-ol bck-ol7' } },
+				{ name: 'Lista 123 azul claro', element: 'ol', attributes: { 'class': 'bck-ol bck-ol8' } },
+				{ name: 'Lista abc verde', element: 'ol', attributes: { 'class': 'bck-ol bck-ol9' } },
+
+				{ name: 'Lista desordenada color', element: 'ul', attributes: { 'class': 'bck-ul bck-ul1' } },
+				{ name: 'Lista desordenada blanca', element: 'ul', attributes: { 'class': 'bck-ul bck-ul2' } },
+
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
+
+				{ name: 'Énfasis 1', element: 'span', attributes: { 'class': 'bck-enfasis-1'} },
+				{ name: 'Énfasis 2', element: 'span', attributes: { 'class': 'bck-enfasis-2'} },
+				{ name: 'Énfasis 3', element: 'span', attributes: { 'class': 'bck-enfasis-3'} },
+				{ name: 'Énfasis 4', element: 'span', attributes: { 'class': 'bck-enfasis-4'} },
+				{ name: 'Énfasis 5', element: 'span', attributes: { 'class': 'bck-enfasis-5'} },
+                
+				{ name: 'Celda encabezado', element: 'td', attributes: { 'class': 'bck-td-1' } }
+			]
+		},
+
+		init: function (scope) {
+			var that = scope || this;
+			//BK-15873 Utilizamos this.parent declarada al inicio de la clase
+			this.parent.init.call(that);
+			that.addActivityTitle();
+			if(window.esWeb) return;
+			that.removeFinalSlide();
+			that.handleScrollEnd();
+			that.setTooltip();
+			window.editar && that.configEditor();
+
+			if ($('.navbar-bottom').length > 0) {
+ 				$('.navbar-bottom ol').wrapAll('<div id="bottom-navigator"></div>');
+		 		var width = 0;
+		 		$('.navbar-bottom li').each(function(i, elem){ width += $(elem).outerWidth(true); });
+		 		$('.navbar-bottom ol').css('width', width * 1.1);
+		 		var scroll = new IScroll('#bottom-navigator', {
+		 			scrollX: true,
+		 			scrollY: false,
+		 			eventPassthrough: true
+		 		});
+		 		scroll.on('scrollEnd', that.handleScrollEnd);
+		 		that.handleScrollEnd.call(scroll);
+	 		}
+
+		},
+
+		configEditor: function (editor) {
+			editor.dtd.$removeEmpty['span'] = false;
+		},
+
+
+		addActivityTitle: function () {
+			if (!blink.courseInfo || !blink.courseInfo.unit) return;
+			$('.libro-left').find('.title').html(function () {
+				return $(this).html().trim() + ' > ' + blink.courseInfo.unit;
+			});
+		},
+
+		handleScrollEnd: function () {
+ 			$('#bottom-navigator')
+ 				.removeClass('show_left')
+ 				.removeClass('show_right');
+
+ 			if (this.x < 0) {
+ 				$('#bottom-navigator').addClass('show_left');
+ 			}
+ 			if (this.x > this.maxScrollX) {
+ 				$('#bottom-navigator').addClass('show_right');
+ 			}
+
+ 		},
+
+		setTooltip: function () {},
+
+		//BK-15873 Quitamos la funcion getEditorStyles para que la herede de basic
+	};
+
+	maristaschileStyle.prototype = _.extend({}, new blink.theme.styles.basic(), maristaschileStyle.prototype);
+
+	blink.theme.styles.maristaschile = maristaschileStyle;
+
+})( blink );
